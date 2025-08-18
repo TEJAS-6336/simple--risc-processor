78625812463faa0b88b927842a74b833105711dd
TEJAS-6336 <tejasbr610@gmail.com>
2025-08-18 12:12:16 +0530

    Update alu.v

--- alu.v
+++ alu.v
@@ -1,14 +1,15 @@
-module alu(input [7:0] a,b,input[2:0] alu_ctrl,output reg [7:0] result);
+module alu(input [15:0] a,b,input[3:0] alu_ctrl,output reg [15:0] result,output reg carry_out);
 always@(*)
 begin
        case(alu_ctrl)
-       3'b000:result=a+b;
-       3'b001:result=a-b;
-       3'b011:result=a&b;
-       3'b100:result=a|b;
-       3'b101:result=~a;
-       default:result=7'b0;
+       4'b0000:result=a+b;
+       4'b0001:result=a-b;
+       4'b0010:result=a&b;
+       4'b0011:result=a|b;
+       4'b0100:result=~a;
+       default:result=16'b0;
        endcase
 end
 endmodule
+       
        
